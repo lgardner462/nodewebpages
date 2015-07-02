dd14966d3c083efd0378a30ed36673879624589e
lawrence <lgardner462@hcc.edu>
2015-07-02 13:54:51 -0400

    Added lists of node name, location, and name/location. Need to figure out how to route to a selected node and pull that data from the lists without making 290 manual entries.

--- csvConv.py
+++ csvConv.py
@@ -7,14 +7,20 @@ import csv
 
 #Open CSV file
 reader = csv.reader(open(sys.argv[1],'rU'))
-for row in reader:
-	text = row[0:2]
-	
 
-@route('/shownode/<name>')
-def index(name):
+nodeNameLoc = []
+nodeNames = []
+nodeLoc = []
+
+for row in reader:
+	nodeNames.append(row[0])
+	nodeLoc.append(row[1:2])
+	nodeNameLoc.append(row[0:2])
+
+@route('/shownode/<nodeNames>')
+def index(nodeNames):
 #make html into table
-	output = template('how I display a node', rows=text)	
+	output = template('how I display a node', rows=nodeNameLoc)	
 	return output
 	
 run(host='localhost')
